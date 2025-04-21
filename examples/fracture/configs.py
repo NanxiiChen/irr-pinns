b6b7f8aab406b550e90f1d95f0c93bf10a357193
Nanxi Chen <nxchen@tongji.edu.cn>
2025-04-21 17:38:39 +0800

    update fracture

--- examples/fracture/configs.py
+++ examples/fracture/configs.py
@@ -23,8 +23,8 @@ class Config:
     # TS = [0.0000, 0.3000, 0.7000, 0.7400, 0.7800]
     TS = [0.0000, 0.2500, 0.5000, 0.8000, 1.0000]
 
-    NUM_LAYERS = 10
-    HIDDEN_DIM = 256
+    NUM_LAYERS = 6
+    HIDDEN_DIM = 200
     OUT_DIM = 3
 
     ACT_NAME = "gelu"
--- examples/fracture/model.py
+++ examples/fracture/model.py
@@ -45,7 +45,7 @@ class PINN(nn.Module):
         sol = self.model.apply(params, x, t)
         phi, disp = jnp.split(sol, [1], axis=-1)
         disp = disp * t / self.cfg.DISP_PRE_SCALE
-        phi = jnp.exp(-phi**2*5)
+        phi = jnp.exp(-phi**2*10)
         return phi, disp
 
     @partial(jit, static_argnums=(0,))
@@ -148,15 +148,15 @@ class PINN(nn.Module):
         else:
             raise ValueError(f"Unknown PDE name: {pde_name}")
 
-        # nabla_phi_fn = jax.jacrev(
-        #     lambda params, x, t: self.net_u(params, x, t)[0], argnums=1
-        # )
-        # nabla_phi = vmap(
-        #     lambda params, x, t: nabla_phi_fn(params, x, t)[0], in_axes=(None, 0, 0)
-        # )(params, x, t)
-        # grad_phi = jax.lax.stop_gradient(jnp.linalg.norm(nabla_phi, ord=2, axis=-1))
-        # weights = 1 / (1 + grad_phi)
-        # residual = weights * residual
+        nabla_phi_fn = jax.jacrev(
+            lambda params, x, t: self.net_u(params, x, t)[0], argnums=1
+        )
+        nabla_phi = vmap(
+            lambda params, x, t: nabla_phi_fn(params, x, t)[0], in_axes=(None, 0, 0)
+        )(params, x, t)
+        grad_phi = jax.lax.stop_gradient(jnp.linalg.norm(nabla_phi, ord=2, axis=-1))
+        weights = 1 / (1 + grad_phi)
+        residual = weights * residual
 
         if not self.cfg.CAUSAL_WEIGHT:
             return jnp.mean(residual**2), {}
