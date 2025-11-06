1bc9514ef046ea898f86a5592da25534d8839779
Nanxi Chen <nxchen@tongji.edu.cn>
2025-11-06 14:23:30 +0800

    remove unused files

--- pinn/__init__.py
+++ pinn/__init__.py
@@ -13,32 +13,32 @@ from .train import train_step, create_train_state, StaggerSwitch, lbfgs_train_st
 
 
 
-def configure_matplotlib():
-    matplotlib.use("Agg")
-    rcParams.update(
-        {
-            "font.size": 16,
-            "font.family": "serif",
-            "font.serif": ["Palatino Linotype"],
-            "mathtext.fontset": "cm",
-            "xtick.direction": "in",
-            "ytick.direction": "in",
-        }
-    )
+# def configure_matplotlib():
+#     matplotlib.use("Agg")
+#     rcParams.update(
+#         {
+#             "font.size": 16,
+#             "font.family": "serif",
+#             "font.serif": ["Palatino Linotype"],
+#             "mathtext.fontset": "cm",
+#             "xtick.direction": "in",
+#             "ytick.direction": "in",
+#         }
+#     )
 
 
-def load_fonts(font_dir="./fonts/"):
-    font_dir = os.path.abspath(font_dir)
-    if not os.path.exists(font_dir):
-        print(f"Warning: Font directory '{font_dir}' does not exist.")
-        return
-    font_names = os.listdir(font_dir)
-    if not font_names:
-        print(f"Warning: No fonts found in directory '{font_dir}'.")
-        return
-    for font_name in font_names:
-        font_manager.fontManager.addfont(os.path.join(font_dir, font_name))
+# def load_fonts(font_dir="./fonts/"):
+#     font_dir = os.path.abspath(font_dir)
+#     if not os.path.exists(font_dir):
+#         print(f"Warning: Font directory '{font_dir}' does not exist.")
+#         return
+#     font_names = os.listdir(font_dir)
+#     if not font_names:
+#         print(f"Warning: No fonts found in directory '{font_dir}'.")
+#         return
+#     for font_name in font_names:
+#         font_manager.fontManager.addfont(os.path.join(font_dir, font_name))
 
 
-configure_matplotlib()
-load_fonts()
+# configure_matplotlib()
+# load_fonts()
